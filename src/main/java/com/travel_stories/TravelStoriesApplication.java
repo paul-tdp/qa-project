bf23de0b5655055d2f1f23597136a1b681f9b14f
paul-tdp <pgirtavic@gmail.com>
2019-10-31 16:40:44 +0000

    successfully implemented file upload functionality

--- src/main/java/com/travel_stories/TravelStoriesApplication.java
+++ src/main/java/com/travel_stories/TravelStoriesApplication.java
@@ -2,8 +2,16 @@ package com.travel_stories;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.travel_stories.entities.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageProperties.class
+})
+
+
 public class TravelStoriesApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/travel_stories/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.travel_stories.exception;
+
+public class FileStorageException extends RuntimeException {
+	public FileStorageException(String message) {
+        super(message);
+    }
+	
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
