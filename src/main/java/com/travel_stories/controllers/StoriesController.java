091e9f91fe9bb257f3a2fef5dd4784f29762896f
paul-tdp <pgirtavic@gmail.com>
2019-11-01 20:56:29 +0000

    allowing cors

--- src/main/java/com/travel_stories/controllers/StoriesController.java
+++ src/main/java/com/travel_stories/controllers/StoriesController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.travel_stories.repositories.StoriesRepo;
 
 @RestController
 @Transactional
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class StoriesController {
 	@Autowired
 	private StoriesRepo repo;
